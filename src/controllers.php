4bc1b75e89091944017dd9a8921ab41384554f5b
Matteo Magni <matteo@magni.me>
2013-06-14 18:23:50 +0200

    cahnge models for feeds

--- src/controllers.php
+++ src/controllers.php
@@ -24,39 +24,26 @@ $app->error(function (\Exception $e, $code) use ($app) {
 
 
 //Feeds
-$app->get('/api/feeds/{id}', function (Request $request, $id) use ($app, $feeds) {
+$app->get('/api/feeds/{id}', function (Request $request, $id) use ($app, $connection, $db) {
+    $collection_name = 'feeds';
+    $collection = $connection->selectCollection($db, $collection_name);
     if ($id === '') {
+        $cursor = $collection->find();
+        $feeds = array();
+        foreach($cursor as $document) {
+            $feeds[] = $document;
+        }
         $callback = $request->query->get('callback');
         $response = new JsonResponse($feeds,200,array('Content-Type' => 'application/json'));
     } else {
-        if ($id >= count($feeds)) {
-            return new JsonResponse('error', 200, array('Content-Type' => 'application/json'));
-        }
-        $feed = new DOMDocument();
-        $feed->load($feeds[$id]['url']);
+        $cursor = $collection->findOne(array('_id' => new MongoId($id)));
+        //if ($id >= count($feeds)) {
+        //    return new JsonResponse('error', 200, array('Content-Type' => 'application/json'));
+        //}
         $json = array();
-        $json['title'] = $feed->getElementsByTagName('channel')->item(0)->getElementsByTagName('title')->item(0)->firstChild->nodeValue;
-        $json['description'] = $feed->getElementsByTagName('channel')->item(0)->getElementsByTagName('description')->item(0)->firstChild->nodeValue;
-        $json['link'] = $feed->getElementsByTagName('channel')->item(0)->getElementsByTagName('link')->item(0)->firstChild->nodeValue;
-        $items = $feed->getElementsByTagName('item');
-        $json['items'] = array();
-        $i = 0;
+        //@TODO read feed
+        //$feed->load($cursor['url']);
 
-        foreach($items as $item) {
-            $title = $item->getElementsByTagName('title')->item(0)->firstChild->nodeValue;
-            $description = $item->getElementsByTagName('description')->item(0)->firstChild->nodeValue;
-            $pubDate = $item->getElementsByTagName('pubDate')->item(0)->firstChild->nodeValue;
-            $guid = $item->getElementsByTagName('guid')->item(0)->firstChild->nodeValue;
-            $link = $item->getElementsByTagName('link')->item(0)->firstChild->nodeValue;
-
-            $json['item'][$i]['title'] = $title;
-            $json['item'][$i]['description'] = $description;
-            $json['item'][$i]['pubdate'] = $pubDate;
-            $json['item'][$i]['guid'] = $guid;
-            $json['item'][$i]['link'] = $link;
-
-            $i++;
-        }
         $callback = $request->query->get('callback');
         $response = new JsonResponse($json,200,array('Content-Type' => 'application/json'));
     }
--- src/models.php
+++ src/models.php
@@ -3,12 +3,3 @@ require_once __DIR__.'/../config/db.php';
 
 $connection = new Mongo("mongodb://$host:27017");
 $db = $connection->selectDB($db_name);
-
-//feeds
-$collection_name = 'feeds';
-$collection = $connection->selectCollection($db, $collection_name);
-$cursor = $collection->find();
-$feeds = array();
-foreach($cursor as $document) {
-    $feeds[] = $document;
-}
