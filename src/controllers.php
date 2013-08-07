d0df7bba3a6e67bc2e88655283633d6a282893f3
Matteo Magni <matteo@magni.me>
2013-08-07 09:01:09 +0200

    refs #1: Cache simplepie #closed

--- src/controllers.php
+++ src/controllers.php
@@ -34,9 +34,9 @@ $app->get('/1.0/feeds/{id}', function (Request $request, $id) use ($app, $connec
             $feedQuery = array('tag' => $tag);
         }
         $cursor = $collection->find($feedQuery);
+        $cursor->sort(array('title' => 1));
         $feeds = array();
         foreach($cursor as $document) {
-            //$feeds[] = $document;
             $feed['id'] = $document['_id']->{'$id'};
             $feed['title'] = $document['title'];
             $feed['url'] = $document['url'];
@@ -47,33 +47,34 @@ $app->get('/1.0/feeds/{id}', function (Request $request, $id) use ($app, $connec
         $response = new JsonResponse($feeds,200,array('Content-Type' => 'application/json'));
     } else {
         $cursor = $collection->findOne(array('_id' => new MongoId($id)));
-        //if ($id >= count($feeds)) {
-        //    return new JsonResponse('error', 200, array('Content-Type' => 'application/json'));
-        //}
         $json = array();
         $feed = $app['simplepie'];
         $feed->set_feed_url($cursor['url']);
         $feed->set_item_class();
-        $feed->enable_cache(false);
-        //$feed->enable_cache(true);
-        //$feed->set_cache_duration(3600);
-        //$feed->set_cache_location('cache');
-        $feed->init();
-        $feed->handle_content_type();
+        // $feed->enable_cache(false);
+        $feed->enable_cache(true);
+        $feed->set_cache_duration(3600);
+        $feed->set_cache_location('../cache/simplepie');
+        $feedResult = $feed->init();
         $news = array();
-        foreach($feed->get_items() as $item) {
-            $n = array();
-            $n['title'] = $item->get_title();
-            $n['description'] = $item->get_description();
-            $n['date'] = $item->get_date('j-n-Y');
-            $n['author'] = $item->get_author()->get_name();
-            $n['link'] = $item->get_link();
-            $news[] = $n;
+        if ($feedResult) {
+            $feed->handle_content_type();
+            foreach($feed->get_items() as $item) {
+                $n = array();
+                $n['title'] = $item->get_title();
+                $n['description'] = $item->get_description();
+                $n['date'] = $item->get_date('j-n-Y');
+                $n['author'] = $item->get_author()->get_name();
+                $n['link'] = $item->get_link();
+                $news[] = $n;
+            }
+            $response = new JsonResponse($news,200,array('Content-Type' => 'application/json'));
+        } else {
+            $response = new JsonResponse('', 503, array('Content-Type' => 'application/json'));
         }
-        $callback = $request->query->get('callback');
-        $response = new JsonResponse($news,200,array('Content-Type' => 'application/json'));
     }
 
+    $callback = $request->query->get('callback');
     $response->setCallback($callback);
     return $response;
 })->value('id', '');
