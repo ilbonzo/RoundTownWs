0c353ce215fd23db30c91c8ebfd85a02a62ce7f8
Matteo Magni <matteo@magni.me>
2013-08-09 17:00:37 +0200

    Use twig cache, place and place/images

--- src/app.php
+++ src/app.php
@@ -7,6 +7,10 @@ use SimplePie as SP;
 require_once '../modules/eden/library/eden.php';
 require_once '../modules/eden/library/eden/foursquare.php';
 
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'prod'));
+
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
@@ -14,7 +18,7 @@ $app->register(new ValidatorServiceProvider());
 $app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/../config/settings.yml'));
 $app->register(new TwigServiceProvider(), array(
     'twig.path'    => array(__DIR__.'/../views'),
-    'twig.options' => array('cache' => __DIR__.'/../cache'),
+    'twig.options' => array('cache' => $app['config']['env'][APPLICATION_ENV]['cache'])
 ));
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
--- src/controllers.php
+++ src/controllers.php
@@ -5,7 +5,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-
 //home page
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html', array());
@@ -22,9 +21,11 @@ $app->error(function (\Exception $e, $code) use ($app) {
     return new Response($app['twig']->render($page, array('code' => $code)), $code);
 });
 
-
 //Feeds
-$app->get('/1.0/feeds/{id}', function (Request $request, $id) use ($app, $connection, $db) {
+/**
+ * Feeds
+ */
+$app->get('/'. $app['config']['app']['api_version'] .'/feeds/{id}', function (Request $request, $id) use ($app, $connection, $db) {
     $collection_name = 'feeds';
     $collection = $connection->selectCollection($db, $collection_name);
     if ($id === '') {
@@ -43,15 +44,13 @@ $app->get('/1.0/feeds/{id}', function (Request $request, $id) use ($app, $connec
             $feed['tag'] = $document['tag'];
             $feeds[] = $feed;
         }
-        $callback = $request->query->get('callback');
-        $response = new JsonResponse($feeds,200,array('Content-Type' => 'application/json'));
+        $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $feeds]), 200,array('Content-Type' => 'application/json'));
     } else {
         $cursor = $collection->findOne(array('_id' => new MongoId($id)));
         $json = array();
         $feed = $app['simplepie'];
         $feed->set_feed_url($cursor['url']);
         $feed->set_item_class();
-        // $feed->enable_cache(false);
         $feed->enable_cache(true);
         $feed->set_cache_duration(3600);
         $feed->set_cache_location('../cache/simplepie');
@@ -68,20 +67,20 @@ $app->get('/1.0/feeds/{id}', function (Request $request, $id) use ($app, $connec
                 $n['link'] = $item->get_link();
                 $news[] = $n;
             }
-            $response = new JsonResponse($news,200,array('Content-Type' => 'application/json'));
+            $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $news]), 200,array('Content-Type' => 'application/json'));
         } else {
             $response = new JsonResponse('', 503, array('Content-Type' => 'application/json'));
         }
     }
 
-    $callback = $request->query->get('callback');
-    $response->setCallback($callback);
     return $response;
 })->value('id', '');
 
-
 //foursquare
-$app->get('1.0/images',function (Request $request) use ($app) {
+/**
+ * images
+ */
+$app->get('/'. $app['config']['app']['api_version'] .'/images',function (Request $request) use ($app) {
 
     $venue = $app['foursquare']->venue($app['config']['foursquare']['access_token']);
     $result = $venue->getVenuePhoto($app['config']['foursquare']['venue_id'],'venue');
@@ -93,13 +92,88 @@ $app->get('1.0/images',function (Request $request) use ($app) {
         $p['user'] = $item['user']['firstName'] . $lastName;
         $photos[] = $p;
     }
-    $callback = $request->query->get('callback');
-    $response = new JsonResponse($photos,200,array('Content-Type' => 'application/json'));
-    $response->setCallback($callback);
+    $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $photos]), 200,array('Content-Type' => 'application/json'));
     return $response;
 });
 
+/**
+ * Places
+ */
+$app->get('/'. $app['config']['app']['api_version'] .'/places/{id}', function (Request $request, $id) use ($app) {
+    $venue = $app['foursquare']->venue($app['config']['foursquare']['access_token']);
+    if ($id === '') {
+        $venue->setLimit(50);
+        $venue->setRadius(2000);
+        $result = $venue->search(null, $app['config']['geo']['latitude'], $app['config']['geo']['longitude']);
 
+        $places = array();
+        foreach ($result['response']['venues'] as $item) {
+
+            isset($item['location']['address']) ? $address = $item['location']['address'] : $address = '';
+            isset($item['contact']['phone']) ? $phone = $item['contact']['phone'] : $phone = '';
+            isset($item['contact']['twitter']) ? $twitter = $item['contact']['twitter'] : $twitter = '';
+            isset($item['url']) ? $url = $item['url'] : $url = '';
+            $p = [
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'address' => $address,
+                'phone' => $phone,
+                'twitter' => $twitter,
+                'url' => $url,
+                'foursquare' => $item['canonicalUrl']
+            ];
+            $places[] = $p;
+        }
+        $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $places]), 200,array('Content-Type' => 'application/json'));
+    } else {
+        $result = $venue->getVenue($id);
+        $item = $result['response']['venue'];
+        isset($item['location']['address']) ? $address = $item['location']['address'] : $address = '';
+        isset($item['contact']['phone']) ? $phone = $item['contact']['phone'] : $phone = '';
+        isset($item['contact']['twitter']) ? $twitter = $item['contact']['twitter'] : $twitter = '';
+        isset($item['url']) ? $url = $item['url'] : $url = '';
+        if ($item['tips']['count'] > 0) {
+            $tips = $item['tips']['groups'][0]['items'];
+        } else {
+            $tips = '';
+        }
+        $place = [
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'address' => $address,
+                'phone' => $phone,
+                'twitter' => $twitter,
+                'url' => $url,
+                'foursquare' => $item['canonicalUrl'],
+                'tips' => $tips
+            ];
+        $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $place]), 200,array('Content-Type' => 'application/json'));
+    }
+
+    return $response;
+})->value('id', '');
+
+/**
+ * Place images
+ */
+$app->get('/'. $app['config']['app']['api_version'] .'/places/{id}/images', function (Request $request, $id) use ($app) {
+    $venue = $app['foursquare']->venue($app['config']['foursquare']['access_token']);
+    $result = $venue->getVenuePhoto($id,'venue');
+    $photos = array();
+    foreach ($result['response']['photos']['items'] as $item) {
+        $p['url'] = $item['prefix'] . 'width960' . $item['suffix'];
+        $p['url_thumb'] = $item['prefix'] . '150x150' . $item['suffix'];
+        isset($item['user']['lastName']) ? $lastName = ' ' . $item['user']['lastName'] : $lastName = '';
+        $p['user'] = $item['user']['firstName'] . $lastName;
+        $photos[] = $p;
+    }
+    $response = new Response($app['twig']->render($app['config']['template']['json'], ['data' => $photos]), 200,array('Content-Type' => 'application/json'));
+    return $response;
+});
+
+/**
+ * Utility
+ */
 $app->get('/utility/foursquare/oauth', function () use ($app) {
     $auth = $app['foursquare']->auth($app['config']['foursquare']['client_id'], $app['config']['foursquare']['client_secret'], $app['config']['foursquare']['uri_redirect']);
     //if no code and no session
